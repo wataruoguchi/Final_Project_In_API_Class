6ab8ab8fb99720dd966b8a25700d24d7517a156e
wataruoguchi <wataru.oguchi.0to1@gmail.com>
2015-02-18 16:05:51 -0800

    Use async to handle 3 asynchronous events
    add async to handle APIs.

--- index.js
+++ index.js
@@ -45,78 +45,92 @@ server.views({
 server.route({
     method: 'GET',
     path:'/quiz',
-    handler: function (request, reply) {
-    	//get credentials
+    handler: function(request, reply) {
     	var credentials = require('./credentials.js'),
-		flickrQueryString = {
-    	    		"method": 'flickr.photos.search',
-    	    		"api_key": credentials.flickr.api_key,
-    	    		"tags": 'vancouver',
-    	    		"format": 'json',
-    	    		"nojsoncallback": 1
-    	    	},
-		httpRequest = require('request');
+    	async = require('async');
+		async.parallel([
+			/* First endpoint (Flickr) */
+			function(callback){
+				var flickrQueryString = {
+		    	    		"method": 'flickr.photos.search',
+		    	    		"api_key": credentials.flickr.api_key,
+		    	    		"tags": 'vancouver',
+		    	    		"format": 'json',
+		    	    		"nojsoncallback": 1
+		    	    	},
+				httpRequest = require('request');
+				httpRequest({
+					method: 'GET',
+					url: 'https://api.flickr.com/services/rest/',
+					qs: flickrQueryString,
+					json: "true"
+				}, function(error, incomingMessage, response){
+					if (!error && incomingMessage.statusCode == 200) {
+						var photos = response.photos.photo,
+						flickrArray = [], flickerUri,
+						resultArray = [], resCode;
 
-    	//request to flickr
-		httpRequest({
-			method: 'GET',
-			url: 'https://api.flickr.com/services/rest/',
-			qs: flickrQueryString,
-			json: "true"
-		}, 
-    	//get response by frickr
-		function (error, incomingMessage, response) {
-			if (!error && incomingMessage.statusCode == 200) {
-				var photos = response.photos.photo,
-				flickrArray = [], flickerUri,
-				resultArray = [], resCode,
-				facebookArray = [], facebookUri,
-				twitterArray = [], tweetTxt;
-
-				// produce flickr photo uri
-				photos.forEach(function(photo, idx){
-					flickerUri = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
-					if(idx<4) {
-						flickrArray.push({"uri": flickerUri, "index": idx});
-					} else if(idx<64+4) {
-						// generate quaternary code
-						resCode = ('00'+((idx-4).toString(4))).slice(-3);
-						resultArray.push({"result": flickerUri, "resCode": resCode});
+						// produce flickr photo uri
+						photos.forEach(function(photo, idx){
+							flickerUri = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg";
+							if(idx<4) {
+								flickrArray.push({"uri": flickerUri, "index": idx});
+							} else if(idx<64+4) {
+								// generate quaternary code
+								resCode = ('00'+((idx-4).toString(4))).slice(-3);
+								resultArray.push({"result": flickerUri, "resCode": resCode});
+							}
+						});
+						callback(false, {"photos": flickrArray, "results": resultArray});
 					}
 				});
-
-				// TODO: I don't like nested...
-				//get response by facebook.
-				var endpoint = "http://graph.facebook.com/", id = "vancouver.institute.of.media.arts", 
+			},
+			/* Second endpoint (Facebook) */
+			function(callback){
+				var facebookArray = [], facebookUri,
+				endpoint = "http://graph.facebook.com/", id = "vancouver.institute.of.media.arts", 
 				facebookHttpRequest = require('request');
 				facebookHttpRequest({
 					method: 'GET',
 					url: endpoint+id+'/photos',
 					json: "true"
-				},
-				function(error, incomingMessage, response){
-		        	var likers = response.data[0].likes.data;
-		        	likers.forEach(function(liker, index){
-		        		if(index<4) {
-			        		facebookUri = endpoint+liker.id+'/picture';
-			        		facebookArray.push({"fb": facebookUri, "index": index});		        			
-		        		}
-		        	});
-
-					// TODO: Nested Ansync. I don't like this.
-					//get response by twitter.
-					var Twit = require('twit'),
-					twitterClient = new Twit(credentials.twitter);
-					twitterClient.get('statuses/user_timeline', { screen_name: 'vanarts', count: 4 },  function (err, data, response) {
-						data.forEach(function(record, idx){
-							tweetTxt = record.text;
-							twitterArray.push({"tweet": tweetTxt, "tweetDate": record.created_at, "index": idx});
-						});
-						//Using template
-						reply.view('./tmplProject.html',{"photos": flickrArray, "twt": twitterArray, "facebook": facebookArray, "results": resultArray});
+				}, function(error, incomingMessage, response){
+					if (!error && incomingMessage.statusCode == 200) {
+			        	var likers = response.data[0].likes.data;
+			        	likers.forEach(function(liker, index){
+			        		if(index<4) {
+				        		facebookUri = endpoint+liker.id+'/picture';
+				        		facebookArray.push({"fb": facebookUri, "index": index});		        			
+			        		}
+			        	});								
+					}
+					callback(false, facebookArray);
+				});
+			},
+			/* Third endpoint (Twitter) */
+			function(callback){
+				var twitterArray = [], tweetTxt,
+				Twit = require('twit'),
+				twitterClient = new Twit(credentials.twitter);
+				twitterClient.get('statuses/user_timeline', { screen_name: 'vanarts', count: 4 },  function (err, data, response) {
+					data.forEach(function(record, idx){
+						tweetTxt = record.text;
+						twitterArray.push({"tweet": tweetTxt, "tweetDate": record.created_at, "index": idx});
 					});
+					callback(false, twitterArray);
 				});
 			}
-		});
-    }
+			],
+			/*Collect results*/
+			function(err, results){
+				if(err) {
+					console.log(err);
+					reply.send(500,"server error");
+					return;
+				}
+				//Using template
+				reply.view('./tmplProject.html',{"photos": results[0].photos,"facebook": results[1], "twt": results[2], "results": results[0].results});
+			}
+		);
+	}
 });
